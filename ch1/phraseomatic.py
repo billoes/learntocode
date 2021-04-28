bca5d63d72d3950bbdf3322a0d4d2e055dbc3876
billoes <oesterheldb@gmail.com>
2021-04-28 15:05:08 +0200

    adding python file

---
+++ ch1/phraseomatic.py
@@ -0,0 +1,10 @@
+#let pythob know we'll be using some random
+#funtionality by importing the random module
+import random
+
+# make three lists, one of verbs, one of adjectives,
+#  and one of nouns
+
+verbs = ['Leverage','Sync','Target','Gamify','Offine',
+        'Crowd-sourced','24/7','Lean-in','30,000 foot']
+
