15daeee158befca8530b0a8252c4c33c30377822
billoes <oesterheldb@gmail.com>
2021-05-01 13:42:31 +0200

    added dog calc to ch1

---
+++ ch1/dogagecalculator.py
@@ -0,0 +1,24 @@
+#Dog Age Calculator:
+#1-ask the user for the dog's name.
+# prompt the user to enter the dog's name,store in dog_name.
+#2-ask the user for the age of the dog in years.
+# prompt the user to enter the dog's age,store in dog_years.
+#3-Multiply the dog’s age by the number 7 to get 
+# the dog’s age in human years.
+# human_years = dog_years x 7.
+
+#4-output dog's name and age in human years.
+#
+# example: Your dog, [name] is
+#[human_years] years old in human years.
+#
+#==============================================
+
+dog_name = input("What is your dog's name?")
+
+dog_age = input("What is your dog's age?")
+
+human_years = dog_age * 7
+
+print()
+
