94e833079a6180ca390e6ea3a5f5b87d2151cdf4
billoes <oesterhedb@gmail.com>
2021-05-13 18:11:35 +0200

    added rock.py

---
+++ ch3/rock.py
@@ -0,0 +1,13 @@
+# program to play a game of siscor,paper, rock with a computer
+#
+#-computer makes a random choice.
+#-prompts user to make a choice.
+# - compare
+#- evaluate and determin if tie ; computer or user wins
+#- print winner
+# end the program
+import random
+
+computer_choice = random.randint(0,2)
+
+print ('The computer choses', computer_choice)
