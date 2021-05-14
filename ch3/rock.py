1c3beed12d8638c3f88eead43ec731f97a5ac23f
billoes <oesterheldb@gmail.com>
2021-05-14 15:43:18 +0200

    updated rock.py

--- ch3/rock.py
+++ ch3/rock.py
@@ -10,13 +10,35 @@ import random
 
 random_choice = random.randint(0,2)
 computer_choice =''
+winner ='it is a tie'
 
 
-print ('The computer chooses', random_choice)
+
+print ('The computer choses', random_choice)
 if random_choice == 0:
     computer_choice = 'rock'
 elif random_choice == 1:
     computer_choice = 'paper'
 else:
     computer_choice = 'scissors'
-print ('The computer choooses', computer_choice)
+print ('The computer chose', computer_choice)
+
+user_choice = input('rock, paper or scissors? ')
+print('You chose', user_choice, 'and the computer chose', computer_choice)
+
+if computer_choice == user_choice:
+    winner = 'it is a tie'
+ #   print('it is a tie.')
+elif computer_choice == 'rock'and user_choice != 'paper':
+    winner='computer'
+elif computer_choice =='paper' and user_choice != 'scissors':
+    winner='computer'
+
+elif computer_choice =='scissors' and user_choice != 'rock':
+    winner='computer'
+#elif computer_choice == user_choice:
+#    winner ='it is a tie'     
+else:
+    winner = 'you'
+
+print('The winner is,', winner,'.')
