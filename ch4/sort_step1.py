2a1e11099ad2098f3ffe491ce2d758ae907862c9
billoes <oesterheldb@gmail.com>
2021-06-07 16:50:43 +0200

    minor changes

--- ch4/sort_step1.py
+++ ch4/sort_step1.py
@@ -1,15 +1,18 @@
 def bubble_sort(scores,numbers):
     swapped = True
-    for i in range(0,len(scores)-1):
     
-        if scores[i] < scores[i+1]:
-            temp = scores[i]
-            scores[i] = scores[i+1]
-            scores[i+1] = temp
-            temp = numbers[i]
-            numbers[i] = numbers[i+1]
-            numbers[i+1] = temp
-            swapped = True
+    while swapped:
+        swapped = False
+        
+        for i in range(0,len(scores)-1):
+            if scores[i] < scores[i+1]:
+                temp = scores[i]
+                scores[i] = scores[i+1]
+                scores[i+1] = temp
+                temp = numbers[i]
+                numbers[i] = numbers[i+1]
+                numbers[i+1] = temp
+                swapped = True
 
 
 scores = [60, 50, 60, 58, 54, 54,
@@ -20,7 +23,14 @@ scores = [60, 50, 60, 58, 54, 54,
           41, 53, 55, 61, 51, 44]
 
 number_of_scores = len(scores)
-soution_numbers = list(range(number_of_scores))
+solution_numbers = list(range(number_of_scores))
 
-bubble_sort(scores, soution_numbers)
+bubble_sort(scores, solution_numbers)
+print('Top Bubble Solutions')
+
+for i in range (0,5):
+    print(str(i+1) +')',
+    'Bubble solution #' + str(solution_numbers[i]),
+    'score', scores[i])
+
 
