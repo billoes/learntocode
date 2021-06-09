10093f99eee848b3ac4124acb4ea5580bc5c215f
billoes <oesterhedb@gmail.com>
2021-06-09 13:51:23 +0200

    created dev_ch6 branch and added file count_syllables.py

---
+++ count_syllables.py
@@ -0,0 +1,18 @@
+countSyllables(word):
+    vowels = "aeiouy"
+    numVowels = 0
+    lastWasVowel = False
+    for wc in word:
+        foundVowel = False
+        for v in vowels:
+            if v == wc:
+                if not lastWasVowel: numVowels+=1   #don't count diphthongs
+                foundVowel = lastWasVowel = True
+                        break
+        if not foundVowel:  #If full cycle and no vowel found, set lastWasVowel to false
+            lastWasVowel = False
+    if len(word) > 2 and word[-2:] == "es": #Remove es - it's "usually" silent (?)
+        numVowels-=1
+    elif len(word) > 1 and word[-1:] == "e":    #remove silent e
+        numVowels-=1
+    return numVowels
