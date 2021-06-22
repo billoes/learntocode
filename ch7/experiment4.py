a984f3b0ec299d914f3f865a75b0658660c1a98f
billoes <oesterheldb@gmail.com>
2021-06-22 14:34:47 +0200

    additional turle experiments

--- ch7/experiment4.py
+++ ch7/experiment4.py
@@ -2,9 +2,9 @@ import turtle
 
 slowpoke = turtle.Turtle()
 slowpoke.shape('turtle')
-slowpoke.color('blue')
 pokey = turtle.Turtle()
 pokey.shape('turtle')
+slowpoke.color('blue')
 pokey.color('red')
 
 def make_square(the_turtle):
@@ -12,17 +12,13 @@ def make_square(the_turtle):
         the_turtle.forward(100)
         the_turtle.right(90)
 
-
 def make_spiral(the_turtle):
-    for i in range(0,36):
+    for i in range(0, 36):
         make_square(the_turtle)
-        the_turtle.rotate(10)
-
+        the_turtle.right(10)
 
 make_spiral(slowpoke)
 pokey.right(5)
 make_spiral(pokey)
 
-
 turtle.mainloop()
-
