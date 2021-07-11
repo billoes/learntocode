03a27188b675bb2cfde5d6ce21054ec06abb4add
billoes <oesterhedb@gmail.com>
2021-07-11 17:36:44 +0200

    added fibonnacci.py

--- ch8/palindrome.py
+++ ch8/palindrome.py
@@ -2,8 +2,11 @@ def is_palidrome(word):
     if len(word) <= 1:
         return True
     else:
-        if word[0] == word[-1]:
-            return is_palidrome(word[1:-1])
+        first = word[0]
+        last = word[-1]
+        middle = word[1:-1]
+        if first == last:
+            return is_palidrome(middle)
         else:
             return False
 
