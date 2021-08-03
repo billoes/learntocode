fe50bbddaa2ce916b773cd0547046d2f61f6beae
billoes <oesterhedb@gmail.com>
2021-08-03 12:40:59 +0200

    started ch9

---
+++ ch9/crazy_step1.py
@@ -0,0 +1,12 @@
+# Crazy Libs
+#Pseudo code
+# Start with creating a template - contains text with placeholders for NOUN,VERB,and ADJECTIVE inside the text.
+#-Load the text file "lib.txt" from typing import List
+#from disk.
+#-iterate through the text file looking for a placeholder.
+#- if placeolder is found evaluate ans ask the user for input. 
+#    --store the answer in a List and contine searching.
+#- if end of the textfile is reached.
+#    -- iterate through the text file and replace the placeholer with the stored vaule.
+#- printout the reult.
+#
