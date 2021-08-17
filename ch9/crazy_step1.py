37297af38286917b2933234167ed658e96808425
billoes <oesterheldb@gmail.com>
2021-08-17 11:22:18 +0200

    experiment with crazy_step.py

--- ch9/crazy_step1.py
+++ ch9/crazy_step1.py
@@ -9,4 +9,26 @@
 #       		2 Substitute user's word for the placeholder. 
 #   	B- Otherwise the word is fine; keep it in the story.
 # 3- Store results
-# Take the processed text with the placeholders filed and write it out to a file with the name prepended by "crazy__" .
+# Take the processed text with the placeholders filed and write it out to a file 
+# with the name prepended by "crazy__" .
+
+def make_crazy_lib(filename):
+    file = open(filename, 'r')
+    text = ''
+
+    for line in file:
+        text = text + process_line(line)
+    file.close()
+    return text
+
+def process_line(line):
+    return line
+
+def main():
+    lib = make_crazy_lib('lib.txt')
+    print(lib)
+
+if __name__ == '__main__':
+    main()
+
+
