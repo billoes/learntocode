b1f576d16d8216752e195e6d3090eda40b8d602c
billoes <oesterheldb@gmail.com>
2021-09-11 18:34:51 +0200

    added model_step2.py not randomizing

--- ch11/model_step2.py
+++ ch11/model_step2.py
@@ -13,8 +13,9 @@ grid_model = [0] * height
 next_grid_model = [0] * height
 
 for i in range(height):
-   grid_model[i] = [0] * width
-   next_grid_model[i] = [0] * width
+    grid_model[i] = [0] * width
+    next_grid_model[i] = [0] * width
+
 randomize(grid_model, width, height)
 
 
@@ -22,19 +23,21 @@ def next_gen():
    global grid_model, next_grid_model
 
    for i in range(0, height):
-       for j in range(0, width):
-           cell = 0
-           print('Checking cell', i, j)
-           count = count_neighbors(grid_model, i, j)
+        for j in range(0, width):
+            cell = 0
+            print('Checking cell', i, j)
+            count = count_neighbors(grid_model, i, j)
 
 
-           if grid_model[i][j] == 0:
+            if grid_model[i][j] == 0:
                if count == 3:
                    cell = 1 
-           elif grid_model[i][j] == 1:
-               if count == 2 or count == 3:
+            elif grid_model[i][j] == 1:
+                if count == 2 or count == 3:
                    cell = 1
             next_grid_model[i][j] = cell
+            print('New value is', next_grid_model[i][j])
+
 
     
 temp = grid_model
