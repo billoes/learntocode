00fcf65dad57f82eea535af1fca6973172349877
billoes <oesterheldb@gmail.com>
2021-09-12 14:42:51 +0200

    completed model_step2.py

--- ch11/model_step2.py
+++ ch11/model_step2.py
@@ -3,47 +3,41 @@ import random
 height = 100
 width = 100
 
-def randomize(grid, width, heigth):
-    for i in range (0, height):
-        for j in range(0, width):
-            grid[i][j] = random.randint(0, 1)
-
+def randomize(grid, width, height):
+   for i in range(0, height):
+       for j in range(0, width):
+           grid[i][j] = random.randint(0,1)
 
 grid_model = [0] * height
 next_grid_model = [0] * height
-
 for i in range(height):
-    grid_model[i] = [0] * width
-    next_grid_model[i] = [0] * width
+   grid_model[i] = [0] * width
+   next_grid_model[i] = [0] * width
 
 randomize(grid_model, width, height)
-
 
 def next_gen():
    global grid_model, next_grid_model
 
    for i in range(0, height):
-        for j in range(0, width):
-            cell = 0
-            print('Checking cell', i, j)
-            count = count_neighbors(grid_model, i, j)
+       for j in range(0, width):
+           cell = 0
+           print('Checking cell', i, j)
+           count = count_neighbors(grid_model, i, j)
 
 
-            if grid_model[i][j] == 0:
+           if grid_model[i][j] == 0:
                if count == 3:
                    cell = 1 
-            elif grid_model[i][j] == 1:
-                if count == 2 or count == 3:
+           elif grid_model[i][j] == 1:
+               if count == 2 or count == 3:
                    cell = 1
-            next_grid_model[i][j] = cell
-            print('New value is', next_grid_model[i][j])
+           next_grid_model[i][j] = cell
+           print('New value is', next_grid_model[i][j])
 
-
-    
-temp = grid_model
-grid_model = next_grid_model
-next_grid_model = temp
-
+   temp = grid_model
+   grid_model = next_grid_model
+   next_grid_model = temp
 
 
 def count_neighbors(grid, row, col):
@@ -70,3 +64,4 @@ def count_neighbors(grid, row, col):
 
 if __name__ == '__main__':
     next_gen()
+    
